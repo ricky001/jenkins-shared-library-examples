cc36f197ef5172461be25e71dcf7f924dff03386
Somya Chawla <somyachawlacs2008@gmail.com>
2024-05-22 10:43:46 +0530

    Update Constants.groovy

--- src/org/example/Constants.groovy
+++ src/org/example/Constants.groovy
@@ -4,3 +4,7 @@ class Constants {
   public static final String SLACK_MESSAGE = "Sending Slack Notification..."
   public static final String EMAIL_MESSAGE = "Sending Email..."
 }
+
+def callingMethodExample6(){
+  example6.cloneRepo("Ricky 001")
+}
