e4266e468de10da940eeed7baf751b1410b1d316
Somya Chawla <somyachawlacs2008@gmail.com>
2024-05-26 17:12:55 +0530

    Create MyConstants.groovy

---
+++ src/org/example/MyConstants.groovy
@@ -0,0 +1,10 @@
+package org.example
+
+class MyConstants {
+  public static final String SLACK_MESSAGE = "Sending Slack Notification..."
+  public static final String EMAIL_MESSAGE = "Sending Email..."
+}
+
+def callingMethodExample6(){
+  example6.cloneRepo("Ricky 001")
+}
