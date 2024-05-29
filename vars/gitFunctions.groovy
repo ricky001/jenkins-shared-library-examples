48fc090b2c3eec93aed3e89b69f10e5bd869bd84
Somya Chawla <somyachawlacs2008@gmail.com>
2024-05-29 17:33:00 +0530

    Update gitFunctions.groovy

--- vars/gitFunctions.groovy
+++ vars/gitFunctions.groovy
@@ -1,11 +1,11 @@
 def gitClone(String url,boolean changelog=false,boolean poll=false){
     if (isUnix()){
       echo "Inside Unix"
-      sh "git changelog: ${changelog}, poll: ${poll}, url: '${url}'"
+      git changelog: ${changelog}, poll: ${poll}, url: '${url}'
     }
   else{
     echo "Inside Windows"
-    bat "git changelog: ${changelog}, poll: ${poll}, url: '${url}'"
+      git changelog: ${changelog}, poll: ${poll}, url: '${url}'
     
   }
 }
